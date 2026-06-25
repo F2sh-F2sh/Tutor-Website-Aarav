681a112dcae60288e5bf465bc7c451068fd9d38c
Fisch <112293434+F2sh-F2sh@users.noreply.github.com>
2026-06-25 12:44:59 -0400

    coom

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ console.log("Website loaded");
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://bzcarceljgdwdqlcbotl.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_VNz-PocQFPrNjsap1Asw2g_s7-AkKb4";
+const SUPABASE_URL = "https://hlwtwhietxwatwqppbco.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_VlSuaVmnbSgNLL_zS8W-ug_9OBOcujl";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
 
